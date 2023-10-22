b8dd5816a7d3ec8b51dea94194411f2476525991
yaanwa <148209271+yaanwa@users.noreply.github.com>
2023-10-22 23:51:23 +0800

    Add files via upload

---
+++ 1022/script.js
@@ -0,0 +1,19 @@
+const input = document.querySelector('input')
+const ul = document.querySelector('ul')
+let id = 0
+
+function add() {
+    ul.innerHTML += `
+        <li id='${++id}'>
+            ${id}. ${input.value}
+            <button onclick='del(${id})'>⁛</button>
+        </li>
+        `
+    input.value=''
+    console.log(input.value);
+}
+
+function  del(id) {
+    const li = document.querySelector(`li[id="${id}"]`);
+    li.remove()
+}
